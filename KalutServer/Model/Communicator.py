a5ec7ca1bf4f6685a3cfd8176a873ec5eae7f6ff
Ron Yutkin <ron.yutkin@windowslive.com>
2017-05-10 15:00:04 +0300

    10.5 1500

--- KalutServer/Model/Communicator.py
+++ KalutServer/Model/Communicator.py
@@ -1,4 +1,13 @@
 import mysql.connector as sql
 class Communicator(object):
     def __init__():
-        pass
+        self.connect_to_db()
+        
+    def connect_to_db(self):
+        self.conn = sql.connect(
+            user='kalut',
+            password='!',
+            host='kalut.ml',
+            database='kalut'
+        )
+        self.cur = conn.cursor()
